65c9f8e177451893da6fc99d2b574880e2e0965d
Kristin Anett Remmelgas <74420515+kristinanett@users.noreply.github.com>
2023-02-01 14:55:09 +0000

    Added comments

--- queries/01_01/SELECTStatement.sql
+++ queries/01_01/SELECTStatement.sql
@@ -1,5 +1,11 @@
+-- overall SELECT syntax
+-- SELECT {* / [DISTINCT] column / expression [alias]} from {table};
+
+-- all rows and columns
 SELECT * FROM emp_tab;
 
+-- only epno and name column
 SELECT empno,name FROM emp_tab;
 
+-- epno column, name column and salary column multiplied by 12 and given new name
 SELECT empno,name,salary*12 'Annual Salary' FROM emp_tab;
